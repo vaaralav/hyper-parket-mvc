6deffc0d480fe1f8e7fe190cb3d4a3d9c2c47fa3
Ville Vaarala <ville.l.vaarala@gmail.com>
2018-02-13 23:01:28 +0200

    Add store & routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
+// import 'todomvc-app-css/index.css';
 import { hyper } from 'hyperhtml/esm';
-// this is hyperHTML
-function tick(render) {
-  render`
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
-    </div>
-  `;
-}
-setInterval(tick, 1000, hyper(document.getElementById('todo')));
+import hyperApp from 'hyperhtml-app';
+import TodoStore, { SHOW_ALL } from './model/todo';
+import { devtools } from 'parket';
+
+const localStorageKey = 'hyper-parket-mvc';
+const initialState = localStorage.getItem(localStorageKey)
+  ? JSON.parse(localStorage.getItem(localStorageKey))
+  : {
+      todos: [
+        {
+          text: 'learn Mobx',
+          completed: false,
+          id: 0,
+          __p_model: 'Todo'
+        },
+        {
+          text: 'learn MST',
+          completed: false,
+          id: 1,
+          __p_model: 'Todo'
+        }
+      ]
+    };
+
+const store = TodoStore(initialState);
+
+const app = hyperApp();
+
+const Link = ({ to, label }) => {
+  const link = hyper`<a href="${to}">${label}</a>`;
+  link.addEventListener('click', {
+    handleEvent: function(event) {
+      event.preventDefault();
+      app.navigate(to);
+    }
+  });
+  return link;
+};
+
+app.use(['/', '/:filter'], function serveTodos(ctx) {
+  store.setFilter(ctx.params.filter || SHOW_ALL);
+  hyper(document.getElementById('todo'))`<h1>TODO</h1>
+  <div>
+    ${Link({ to: '/', label: 'all' })}
+    ${Link({ to: '/show_completed', label: 'completed' })}
+    ${Link({ to: '/show_active', label: 'active' })}
+</div><pre>${JSON.stringify(store.filteredTodos, null, 2)}</pre>`;
+});
+
+//
+app.navigate(window.location.pathname);
